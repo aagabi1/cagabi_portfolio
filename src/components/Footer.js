a2a19fd83ed8df3fcd57dba7240bc150d475c5fd
aagabi1 <aagabi1@yahoo.com>
2023-06-19 11:42:13 -0400

    updated footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,21 @@ import "../stylesheets/footer.css";
 function Footer() {
   return (
     <footer>
-      <a className="social-link" href="https://www.instagram.com/topboyxoxo/">
-        <img src={instagram} alt="" />
-      </a>
-      <a className="social-link" href="https://www.linkedin.com/in/aagabi1/">
-        <img src={linkedin} alt="" />
-      </a>
-      <a
-        className="social-link"
-        href="https://www.youtube.com/channel/UC_GCi_Qu7IfNubvqYVWWZeA"
-      >
-        <img src={youtube} alt="" />
-      </a>
+      <div>
+        <a className="social-link" href="https://www.instagram.com/topboyxoxo/">
+          <img src={instagram} alt="" />
+        </a>
+        <a className="social-link" href="https://www.linkedin.com/in/aagabi1/">
+          <img src={linkedin} alt="" />
+        </a>
+        <a
+          className="social-link"
+          href="https://www.youtube.com/channel/UC_GCi_Qu7IfNubvqYVWWZeA"
+        >
+          <img src={youtube} alt="" />
+        </a>
+      </div>
+      <p className="footer-description">Made by Courage Agabi, 2023. </p>
     </footer>
   );
 }
